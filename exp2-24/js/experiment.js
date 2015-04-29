675e589c89c557ec4843f972b2c2d9461800feb2
Djellel Eddine Difallah <djelleleddine.difallah@exascale.info>
2015-04-29 13:59:39 +0200

    adapt

--- exp2-24/js/experiment.js
+++ exp2-24/js/experiment.js
@@ -340,7 +340,7 @@ TaskFSM.prototype = {
                 $('#container-main').html(Template.trialmessage('<p id="cross" style="font-family: Arial, Helvetica, sans-serif; font-size: 32px; color: darkgreen;">Correct, '+this.task.result.time+' ms</p>')); 
                 this.task.result.correct = true;
                 totalCorrect++;
-                rate = 0.015;
+                rate = 0.014;
                 totalBonus = totalBonus + rate; 
                 var bo = Number((totalBonus).toFixed(3));
                 actuator.updateScore(bo);
